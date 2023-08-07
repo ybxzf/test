69da69a790755e425447a65b70b22844f28beb03
ybxzf <42826051+ybxzf@users.noreply.github.com>
2023-08-07 16:50:18 +0800

    更新 test.js

--- test.js
+++ test.js
@@ -23,3 +23,52 @@ var str = 'aaaabbbbssssxcccddddsdfscxxasa'
       console.log('出现的次数是'+max);
     }
   }
+
+.line-container {
+  position: relative;
+}
+.point {
+  position: absolute;
+  border-radius: 50%;
+  background-color: #000;
+}
+.point-1 {
+  top: 50px;
+  left: 50px;
+  width: 20px;
+  height: 20px;
+}
+.point-2 {
+  top: 100px;
+  left: 150px;
+  width: 20px;
+  height: 20px;
+}
+.line {
+  position: absolute;
+  height: 2px;
+  background-color: black;
+}
+.point-1::before {
+  content: "";
+  position: absolute;
+  top: -35%;
+  left: 150%;
+  height: 200%;
+  width: 100px;
+ background-color: #000;
+  transform: rotate(45deg);
+  border-radius: 50%;
+}
+
+.point-2::after {
+  content: "";
+  position: absolute;
+  top: -35%;
+  left: -100%;
+  height: 200%;
+  width: 100px;
+  background-color: #000;
+  transform: rotate(-45deg);
+  border-radius: 50%;
+}
